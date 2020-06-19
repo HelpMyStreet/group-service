81fc07eccca04233da99127a7712137e5dc9d83b
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-06-19 15:09:27 +0100

    add ef migrations

--- GroupService/GroupService.AzureFunction/Startup.cs
+++ GroupService/GroupService.AzureFunction/Startup.cs
@@ -1,12 +1,17 @@
 ﻿using AutoMapper;
+using AutoMapper.Configuration.Conventions;
+using GroupService.Core.Config;
 using GroupService.Core.Interfaces.Repositories;
 using GroupService.Handlers;
 using GroupService.Mappers;
 using GroupService.Repo;
 using MediatR;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Host.Bindings;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System.Reflection;
 
 [assembly: FunctionsStartup(typeof(GroupService.AzureFunction.Startup))]
@@ -16,12 +21,50 @@ namespace GroupService.AzureFunction
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            ExecutionContextOptions executioncontextoptions = builder.Services.BuildServiceProvider()
+           .GetService<IOptions<ExecutionContextOptions>>().Value;
+            string currentDirectory = executioncontextoptions.AppDirectory;
+
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
+            .SetBasePath(currentDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+            IConfigurationRoot config = configBuilder.Build();
+
             builder.Services.AddMediatR(typeof(FunctionAHandler).Assembly);
             builder.Services.AddAutoMapper(typeof(AddressDetailsProfile).Assembly);
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                    options.UseInMemoryDatabase(databaseName: "GroupService.AzureFunction"));
             builder.Services.AddTransient<IRepository, Repository>();
+
+            //ConnectionStrings connectionStrings1 = config.GetSection("ConnectionStrings").Get<ConnectionStrings>();
+
+            IConfigurationSection connectionStringSettings = config.GetSection("ConnectionStrings");
+            builder.Services.Configure<ConnectionStrings>(connectionStringSettings);
+
+            ConnectionStrings connectionStrings = new ConnectionStrings();
+            connectionStringSettings.Bind(connectionStrings);
+
+            // automatically apply EF migrations
+            // DbContext is being created manually instead of through DI as it throws an exception and I've not managed to find a way to solve it yet: 
+            // 'Unable to resolve service for type 'Microsoft.Azure.WebJobs.Script.IFileLoggingStatusManager' while attempting to activate 'Microsoft.Azure.WebJobs.Script.Diagnostics.HostFileLoggerProvider'.'
+            DbContextOptionsBuilder<ApplicationDbContext> dbContextOptionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            ConfigureDbContextOptionsBuilder(dbContextOptionsBuilder, connectionStrings.GroupService);
+            ApplicationDbContext dbContext = new ApplicationDbContext(dbContextOptionsBuilder.Options);
+
+            dbContext.Database.Migrate();
+
+            int i = 1;
+
+        }
+
+        private void ConfigureDbContextOptionsBuilder(DbContextOptionsBuilder options, string connectionString)
+        {
+            options
+                .UseSqlServer(connectionString)
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
         }
     }
 }
