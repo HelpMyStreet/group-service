52b3d35f2323ccdeea0919c5c00f2b23963b743e
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-01 15:18:29 +0100

    Add GroupKey

---
+++ GroupService/GroupService.Repo/Migrations/20200701141739_AddGroupKey.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class AddGroupKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GroupKey",
+                schema: "Group",
+                table: "Group",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "Group",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "GroupKey",
+                value: "Generic");
+
+            migrationBuilder.CreateIndex(
+                name: "UC_GroupKey",
+                schema: "Group",
+                table: "Group",
+                column: "GroupKey",
+                unique: true,
+                filter: "[GroupKey] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UC_GroupKey",
+                schema: "Group",
+                table: "Group");
+
+            migrationBuilder.DropColumn(
+                name: "GroupKey",
+                schema: "Group",
+                table: "Group");
+        }
+    }
+}
