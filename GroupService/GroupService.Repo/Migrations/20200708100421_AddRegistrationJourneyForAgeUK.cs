fef2ee4861552a22738944d570b0c164bc0a18e2
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-08 11:05:27 +0100

    Add journey

---
+++ GroupService/GroupService.Repo/Migrations/20200708100421_AddRegistrationJourneyForAgeUK.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class AddRegistrationJourneyForAgeUK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RegistrationJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -3, "" },
+                column: "RegistrationFormVariant",
+                value: (byte)3);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RegistrationJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -3, "" },
+                column: "RegistrationFormVariant",
+                value: (byte)0);
+        }
+    }
+}
