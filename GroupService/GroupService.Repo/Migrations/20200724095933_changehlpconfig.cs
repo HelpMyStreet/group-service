07bed82bc36a8d8bfb1e3fd463e5371d12b6e53c
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-24 10:59:55 +0100

    ran migration script

---
+++ GroupService/GroupService.Repo/Migrations/20200724095933_changehlpconfig.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class changehlpconfig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -4, "" });
+
+            migrationBuilder.InsertData(
+                schema: "Lookup",
+                table: "RequestHelpFormVariant",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 6, "HLP_CommunityConnector" });
+
+            migrationBuilder.InsertData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                columns: new[] { "GroupID", "Source", "RequestHelpFormVariant", "TargetGroups" },
+                values: new object[] { -4, "connected-together-service-directory", (byte)6, (byte)0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Lookup",
+                table: "RequestHelpFormVariant",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -4, "connected-together-service-directory" });
+
+            migrationBuilder.InsertData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                columns: new[] { "GroupID", "Source", "RequestHelpFormVariant", "TargetGroups" },
+                values: new object[] { -4, "", (byte)1, (byte)0 });
+        }
+    }
+}
