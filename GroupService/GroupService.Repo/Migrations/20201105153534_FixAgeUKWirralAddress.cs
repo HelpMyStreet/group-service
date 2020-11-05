058fd911cce6cbf40679b87aff62e1467bb8cfb7
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-11-05 15:36:09 +0000

    fix age uk wirral address

---
+++ GroupService/GroupService.Repo/Migrations/20201105153534_FixAgeUKWirralAddress.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class FixAgeUKWirralAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "RequestorDetails",
+                keyColumn: "GroupId",
+                keyValue: -7,
+                columns: new[] { "AddressLine3", "Locality" },
+                values: new object[] { "141 Park Road North", "Birkenhead" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "RequestorDetails",
+                keyColumn: "GroupId",
+                keyValue: -7,
+                columns: new[] { "AddressLine3", "Locality" },
+                values: new object[] { "141 Park Road", "North Birkenhead" });
+        }
+    }
+}
