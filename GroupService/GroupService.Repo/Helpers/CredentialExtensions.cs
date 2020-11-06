f60dbc10e10ff19e9448247af0b7053523746814
Mike Matthews <mike@factor-50.co.uk>
2020-11-06 14:06:42 +0000

    Adding mailto to Credential "how to achieve" text

--- GroupService/GroupService.Repo/Helpers/CredentialExtensions.cs
+++ GroupService/GroupService.Repo/Helpers/CredentialExtensions.cs
@@ -73,7 +73,7 @@ namespace GroupService.Repo.Helpers
                 CredentialId = DBS_CHECK,
                 CredentialTypeId = (int)CredentialTypes.ThirdPartyCheck,
                 Name = "DBS Check",
-                HowToAchieve = "Email Age UK Wirral to request or register your DBS check at volunteers@ageukwirral.org.uk",
+                HowToAchieve = "Email Age UK Wirral to request or register your DBS check at mailto:volunteers@ageukwirral.org.uk",
                 HowToAchieve_CTA_Destination = "",
                 WhatIsThis = $"Use this credential to record a completed DBS (Disclosure and Barring Service) check.\r\n\r\n" +
                 $"Volunteer admins should follow internal processes for logging a DBS check.",
@@ -87,7 +87,7 @@ namespace GroupService.Repo.Helpers
                 CredentialId = MANUALLY_VERIFIED,
                 CredentialTypeId = (int)CredentialTypes.IdentityVerification,
                 Name = "Manual ID Verification",
-                HowToAchieve = "If you’re unable to verify with Yoti, email Age UK Wirral to find out how they can check your ID at volunteers@ageukwirral.org.uk",
+                HowToAchieve = "If you’re unable to verify with Yoti, email Age UK Wirral to find out how they can check your ID at mailto:volunteers@ageukwirral.org.uk",
                 HowToAchieve_CTA_Destination = "",
                 WhatIsThis = $"Use this credential to certify that you have verified a volunteer’s identity and are satisfied they are who they claim to be. \r\n\r\n" +
                 $"Volunteer admins should follow internal processes for manually verifying a volunteer's identity.",
