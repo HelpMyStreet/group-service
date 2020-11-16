a33c69cf802c0036e80d3df7ec5cd21f0857a3ce
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-11-16 14:31:37 +0000

    Edit email address

---
+++ GroupService/GroupService.Repo/Migrations/20201116143102_EditEmailAddress.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class EditEmailAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "RequestorDetails",
+                keyColumn: "GroupId",
+                keyValue: -7,
+                column: "EmailAddress",
+                value: "h&cadminteam@ageukwirral.org.uk");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "RequestorDetails",
+                keyColumn: "GroupId",
+                keyValue: -7,
+                column: "EmailAddress",
+                value: "enquiries@ageukwirral.org.uk");
+        }
+    }
+}
