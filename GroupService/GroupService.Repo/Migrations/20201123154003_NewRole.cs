a7356d00a85ce3b62d129aa713b7bc70e06b9419
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-11-23 15:43:40 +0000

    add lookup role

---
+++ GroupService/GroupService.Repo/Migrations/20201123154003_NewRole.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class NewRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "Lookup",
+                table: "Role",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 7, "UserAdmin_ReadOnly" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Lookup",
+                table: "Role",
+                keyColumn: "ID",
+                keyValue: 7);
+        }
+    }
+}
