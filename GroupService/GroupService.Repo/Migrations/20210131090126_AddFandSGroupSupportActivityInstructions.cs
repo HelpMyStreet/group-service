3c55f101a94e6d179cf0746d856c2296b297626e
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-31 09:03:04 +0000

    Add Group Support Activity Instructions for faversham

---
+++ GroupService/GroupService.Repo/Migrations/20210131090126_AddFandSGroupSupportActivityInstructions.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class AddFandSGroupSupportActivityInstructions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "Group",
+                table: "SupportActivityInstructions",
+                columns: new[] { "SupportActivityInstructionsID", "Instructions" },
+                values: new object[,]
+                {
+                    { (short)27, "{\"SupportActivityInstructions\":27,\"ActivityDetails\":null,\"Intro\":null,\"Steps\":[{\"Heading\":\"Give them a call\",\"Detail\":\"Be a friendly voice at the end of the phone and have a good chat.\"},{\"Heading\":\"Mark the request as complete\",\"Detail\":\"When you're finished, mark the request as complete in “My Accepted Requests” - this will let us (and anyone else involved with the request) know it's been completed. You’ll still be able to find their contact details in “My Complete Requests” in case you need to get back in touch. If someone else has requested the help on their behalf it may be useful to give them a call to let them know how it went.\"}],\"Close\":\"If for any reason you can’t complete the request before it’s due, let us know by updating the accepted request and clicking “Can’t Do”.\"}" },
+                    { (short)29, "{\"SupportActivityInstructions\":29,\"ActivityDetails\":null,\"Intro\":null,\"Steps\":[{\"Heading\":\"Contact us (Age UK Faversham & Sittingbourne)\",\"Detail\":\"Please contact mailto:volunteering@ageukfaversham.org.uk to book your meal (you provide the company, we’ll provide the food!).\"},{\"Heading\":\"Enjoy your dinner together\",\"Detail\":\"Use the details given to you to meet up at your chosen mealtime. Have a chat and provide some company for the person you’re joining.\"},{\"Heading\":\"Mark the request as complete\",\"Detail\":\"When you're home, mark the request as complete in “My Accepted Requests”. If you have any concerns about the person you visited let us know immediately by emailing mailto:volunteering@ageukfaversham.org.uk.\"}],\"Close\":\"If for any reason you can’t complete the request before it’s due, let us know by updating the accepted request and clicking “Can’t Do”.\"}" },
+                    { (short)28, "{\"SupportActivityInstructions\":28,\"ActivityDetails\":null,\"Intro\":null,\"Steps\":[{\"Heading\":\"Contact us (Age UK Faversham & Sittingbourne)\",\"Detail\":\"Please contact mailto:volunteering@ageukfaversham.org.uk to arrange for the collection of hot meals and the delivery details.\"},{\"Heading\":\"Deliver the meals\",\"Detail\":\"Use the details given to you to deliver the meals. It would be great if you could also stop to say hello and see how they’re getting on.\"},{\"Heading\":\"Mark the request as complete\",\"Detail\":\"When you're finished, mark the request as complete in “My Accepted Requests”. If you have any concerns about the person you visited let us know immediately by emailing mailto:volunteering@ageukfaversham.org.uk.\"}],\"Close\":\"If for any reason you can’t complete the request before it’s due, let us know by updating the accepted request and clicking “Can’t Do”.\"}" },
+                    { (short)30, "{\"SupportActivityInstructions\":30,\"ActivityDetails\":null,\"Intro\":null,\"Steps\":[{\"Heading\":\"Find out what help is needed\",\"Detail\":\"Make sure you’ve seen all the details by clicking the ‘View more info link’. If you’re able to help, click to accept the request.\"},{\"Heading\":\"Provide the transport\",\"Detail\":\"Use the details given to you to pick up and drop off as required.\"},{\"Heading\":\"Mark the request as complete\",\"Detail\":\"When you're finished, mark the request as complete in “My Accepted Requests”.\"}],\"Close\":\"If for any reason you can’t complete the request before it’s due, let us know by updating the accepted request and clicking “Can’t Do”.\"}" },
+                    { (short)31, "{\"SupportActivityInstructions\":31,\"ActivityDetails\":null,\"Intro\":null,\"Steps\":[{\"Heading\":\"Find out what help is needed\",\"Detail\":\"Make sure you’ve seen all the details by clicking the ‘View more info link’. If you’re able to help, click to accept the request.\"},{\"Heading\":\"Provide the help that is needed\",\"Detail\":\"Use the details given to you to carry out the request.\"},{\"Heading\":\"Mark the request as complete\",\"Detail\":\"When you're finished, mark the request as complete in “My Accepted Requests”.\"}],\"Close\":\"If for any reason you can’t complete the request before it’s due, let us know by updating the accepted request and clicking “Can’t Do”.\"}" },
+                    { (short)32, "{\"SupportActivityInstructions\":32,\"ActivityDetails\":null,\"Intro\":null,\"Steps\":[{\"Heading\":\"Contact us (Age UK Faversham & Sittingbourne)\",\"Detail\":\"Please contact mailto:volunteering@ageukfaversham.org.uk to receive further information about this request.\"},{\"Heading\":\"Provide the help that is needed\",\"Detail\":\"Use the details given to you to carry out the request. It would be great if you could also stop for a quick chat to say hello and see how they’re getting on.\"},{\"Heading\":\"Mark the request as complete\",\"Detail\":\"When you're finished, mark the request as complete in “My Accepted Requests”. If you have any concerns about the person you visited let us know immediately by emailing mailto:volunteering@ageukfaversham.org.uk.\"}],\"Close\":\"If for any reason you can’t complete the request before it’s due, let us know by updating the accepted request and clicking “Can’t Do”.\"}" }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Lookup",
+                table: "SupportActivityInstructions",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 27, "AgeUKFANDS_FriendlyChat" },
+                    { 28, "AgeUKFANDS_MealsToYourDoor" },
+                    { 29, "AgeUKFANDS_MealtimeCompanion" },
+                    { 30, "AgeUKFANDS_Transport" },
+                    { 31, "AgeUKFANDS_VolunteerSupport" },
+                    { 32, "AgeUKFANDS_Other" }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Group",
+                table: "GroupSupportActivityInstructions",
+                columns: new[] { "GroupID", "SupportActivityID", "SupportActivityInstructionsID" },
+                values: new object[,]
+                {
+                    { -13, 7, (short)27 },
+                    { -13, 23, (short)29 },
+                    { -13, 21, (short)28 },
+                    { -13, 16, (short)30 },
+                    { -13, 22, (short)31 },
+                    { -13, 11, (short)32 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityInstructions",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -13, 7 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityInstructions",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -13, 11 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityInstructions",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -13, 16 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityInstructions",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -13, 21 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityInstructions",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -13, 22 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityInstructions",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -13, 23 });
+
+            migrationBuilder.DeleteData(
+                schema: "Lookup",
+                table: "SupportActivityInstructions",
+                keyColumn: "ID",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                schema: "Lookup",
+                table: "SupportActivityInstructions",
+                keyColumn: "ID",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                schema: "Lookup",
+                table: "SupportActivityInstructions",
+                keyColumn: "ID",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                schema: "Lookup",
+                table: "SupportActivityInstructions",
+                keyColumn: "ID",
+                keyValue: 30);
+
+            migrationBuilder.DeleteData(
+                schema: "Lookup",
+                table: "SupportActivityInstructions",
+                keyColumn: "ID",
+                keyValue: 31);
+
+            migrationBuilder.DeleteData(
+                schema: "Lookup",
+                table: "SupportActivityInstructions",
+                keyColumn: "ID",
+                keyValue: 32);
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "SupportActivityInstructions",
+                keyColumn: "SupportActivityInstructionsID",
+                keyValue: (short)27);
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "SupportActivityInstructions",
+                keyColumn: "SupportActivityInstructionsID",
+                keyValue: (short)28);
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "SupportActivityInstructions",
+                keyColumn: "SupportActivityInstructionsID",
+                keyValue: (short)29);
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "SupportActivityInstructions",
+                keyColumn: "SupportActivityInstructionsID",
+                keyValue: (short)30);
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "SupportActivityInstructions",
+                keyColumn: "SupportActivityInstructionsID",
+                keyValue: (short)31);
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "SupportActivityInstructions",
+                keyColumn: "SupportActivityInstructionsID",
+                keyValue: (short)32);
+        }
+    }
+}
