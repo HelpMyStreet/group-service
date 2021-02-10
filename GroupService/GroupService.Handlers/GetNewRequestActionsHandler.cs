e915b3300f57661a5457f043bc97f23fd9845787
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-10 12:13:28 +0000

    Tweak GetAction method

--- GroupService/GroupService.Handlers/GetNewRequestActionsHandler.cs
+++ GroupService/GroupService.Handlers/GetNewRequestActionsHandler.cs
@@ -2,6 +2,7 @@
 using HelpMyStreet.Contracts.GroupService.Request;
 using HelpMyStreet.Contracts.GroupService.Response;
 using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Extensions;
 using HelpMyStreet.Utils.Models;
 using MediatR;
 using System;
@@ -20,6 +21,56 @@ namespace GroupService.Handlers
             _repository = repository;
         }
 
+        private List<int> GetAvailableGroups(GetRequestHelpFormVariantResponse requestJourney, int referringGroupId, string source, CancellationToken cancellationToken)
+        {
+            int targetGroupId;
+            bool includeChildGroups;
+
+            switch (requestJourney.TargetGroups)
+            {
+                case TargetGroups.GenericGroup:
+                    targetGroupId = -1;
+                    includeChildGroups = false;
+                    break;
+                case TargetGroups.ParentGroup:
+                    targetGroupId = _repository.GetGroupById(referringGroupId, cancellationToken).ParentGroupId.Value;
+                    includeChildGroups = false;
+                    break;
+                case TargetGroups.SiblingsAndParentGroup:
+                    targetGroupId = _repository.GetGroupById(referringGroupId, cancellationToken).ParentGroupId.Value;
+                    includeChildGroups = true;
+                    break;
+                case TargetGroups.ThisGroup:
+                    targetGroupId = referringGroupId;
+                    includeChildGroups = false;
+                    break;
+                case TargetGroups.ThisGroupAndChildren:
+                    targetGroupId = referringGroupId;
+                    includeChildGroups = true;
+                    break;
+                default:
+                    throw new Exception($"Unexpected TargetGroups value {requestJourney.TargetGroups}");
+            }
+
+            List<int> targetGroups;
+
+            if (includeChildGroups)
+            {
+                targetGroups = _repository.GetGroupAndChildGroups(targetGroupId, cancellationToken);
+
+                if (targetGroups == null)
+                {
+                    throw new Exception("Null response from GetGroupAndChildGroups");
+                }
+            }
+            else
+            {
+                targetGroups = new List<int>() { targetGroupId };
+            }
+
+            return targetGroups;
+        }
+
         public async Task<GetNewRequestActionsResponse> Handle(GetNewRequestActionsRequest request, CancellationToken cancellationToken)
         {
             Dictionary<Guid, TaskAction> actions = new Dictionary<Guid, TaskAction>();
@@ -32,6 +83,8 @@ namespace GroupService.Handlers
                 throw new Exception("null response from GetRequestHelpFormVariant");
             }
 
+            List<int> targetGroups = GetAvailableGroups(requestJourney, request.HelpRequest.ReferringGroupId, request.HelpRequest.Source, cancellationToken);
+
             if (requestJourney.RequestsRequireApproval)
             {
                 requestTaskActions.Add(NewTaskAction.NotifyGroupAdmins, new List<int> { request.HelpRequest.ReferringGroupId });
@@ -41,61 +94,15 @@ namespace GroupService.Handlers
                 requestTaskActions.Add(NewTaskAction.SetStatusToOpen, null);
             }
             requestTaskActions.Add(NewTaskAction.SendRequestorConfirmation, null);
+            requestTaskActions.Add(NewTaskAction.MakeAvailableToGroups, targetGroups);
+
+            RequestType requestType = request.NewJobsRequest.Jobs.First().SupportActivity.RequestType();
 
             foreach (Job j in request.NewJobsRequest.Jobs)
             {
                 TaskAction taskAction = new TaskAction() { TaskActions = new Dictionary<NewTaskAction, List<int>>() };
-                
-                bool faceMaskRequest = j.SupportActivity == SupportActivities.FaceMask;
-               
-                int targetGroupId;
-                bool includeChildGroups;
 
-                switch (requestJourney.TargetGroups)
-                {
-                    case TargetGroups.GenericGroup: 
-                        targetGroupId = -1;
-                        includeChildGroups = false;
-                        break;
-                    case TargetGroups.ParentGroup: 
-                        targetGroupId = _repository.GetGroupById(request.HelpRequest.ReferringGroupId, cancellationToken).ParentGroupId.Value;
-                        includeChildGroups = false;
-                        break;
-                    case TargetGroups.SiblingsAndParentGroup:
-                        targetGroupId = _repository.GetGroupById(request.HelpRequest.ReferringGroupId, cancellationToken).ParentGroupId.Value;
-                        includeChildGroups = true;
-                        break;
-                    case TargetGroups.ThisGroup:
-                        targetGroupId = request.HelpRequest.ReferringGroupId;
-                        includeChildGroups = false;
-                        break;
-                    case TargetGroups.ThisGroupAndChildren:
-                        targetGroupId = request.HelpRequest.ReferringGroupId;
-                        includeChildGroups = true;
-                        break;
-                    default:
-                        throw new Exception($"Unexpected TargetGroups value {requestJourney.TargetGroups}");
-                }
-                
-                List<int> targetGroups;
-
-                if (includeChildGroups)
-                {
-                    targetGroups = _repository.GetGroupAndChildGroups(targetGroupId, cancellationToken);
-
-                    if (targetGroups == null)
-                    {
-                        throw new Exception("Null response from GetGroupAndChildGroups");
-                    }
-                }
-                else
-                {
-                    targetGroups = new List<int>() { targetGroupId };
-                }
-
-                taskAction.TaskActions.Add(NewTaskAction.MakeAvailableToGroups, targetGroups);                
-
-                if (!requestJourney.RequestsRequireApproval)
+                if (!requestJourney.RequestsRequireApproval && requestType == RequestType.Task)
                 {
                     taskAction.TaskActions.Add(NewTaskAction.NotifyMatchingVolunteers, targetGroups);
                 }
