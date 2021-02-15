dad93d6a7cc50dd786505fbdcc7ef0620da2fdac
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-15 10:53:52 +0000

    add GetRegistrationFormVariant

---
+++ GroupService/GroupService.Handlers/GetSupportActivitiesConfigurationHandler.cs
@@ -0,0 +1,29 @@
+﻿using GroupService.Core.Interfaces.Repositories;
+using HelpMyStreet.Contracts.GroupService.Request;
+using HelpMyStreet.Contracts.GroupService.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GroupService.Handlers
+{
+    public class GetSupportActivitiesConfigurationHandler : IRequestHandler<GetSupportActivitiesConfigurationRequest, GetSupportActivitiesConfigurationResponse>
+    {
+        private readonly IRepository _repository;
+        public GetSupportActivitiesConfigurationHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetSupportActivitiesConfigurationResponse> Handle(GetSupportActivitiesConfigurationRequest request, CancellationToken cancellationToken)
+        {
+            List<HelpMyStreet.Utils.Models.SupportActivityConfiguration> config = _repository.GetSupportActivitiesConfiguration(cancellationToken);
+            return new GetSupportActivitiesConfigurationResponse()
+            {
+                SupportActivityConfigurations = config
+            };
+        }
+    }
+}
