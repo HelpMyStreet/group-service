887da66cffe24504c779521243439e33a4294d85
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-23 10:43:28 +0000

    take default away from shopping fix wording

---
+++ GroupService/GroupService.Repo/Migrations/20210223104103_remoeDefaultShopping.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class remoeDefaultShopping : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "SupportActivityInstructions",
+                keyColumn: "SupportActivityInstructionsID",
+                keyValue: (short)33,
+                column: "Instructions",
+                value: "{\"SupportActivityInstructions\":33,\"ActivityDetails\":null,\"Intro\":null,\"Steps\":[{\"Heading\":\"Find out what help is needed\",\"Detail\":\"Make sure you’ve seen all the details by clicking the ‘View more info' link. If you’re able to help, click to accept the request.\"},{\"Heading\":\"Provide the help that is needed\",\"Detail\":\"Use the details given to you to carry out the request.\"},{\"Heading\":\"Mark the request as complete\",\"Detail\":\"When you're finished, mark the request as complete in “My Requests”.\"}],\"Close\":\"If for any reason you can’t complete the request before it’s due, let us know by updating the accepted request and clicking “Can’t Do”.\"}");
+
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RegistrationFormSupportActivity",
+                keyColumns: new[] { "RequestHelpFormVariantID", "SupportActivityID" },
+                keyValues: new object[] { (byte)0, 1 },
+                column: "IsPreSelected",
+                value: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "SupportActivityInstructions",
+                keyColumn: "SupportActivityInstructionsID",
+                keyValue: (short)33,
+                column: "Instructions",
+                value: "{\"SupportActivityInstructions\":33,\"ActivityDetails\":null,\"Intro\":null,\"Steps\":[{\"Heading\":\"Find out what help is needed\",\"Detail\":\"Make sure you’ve seen all the details by clicking the ‘View more info' link. If you’re able to help, click to accept the request.\"},{\"Heading\":\"Provide the help that is needed\",\"Detail\":\"Use the details given to you to carry out the request.\"},{\"Heading\":\"Mark the request as complete\",\"Detail\":\"When you're finished, mark the request as complete in “My Accepted Requests”.\"}],\"Close\":\"If for any reason you can’t complete the request before it’s due, let us know by updating the accepted request and clicking “Can’t Do”.\"}");
+
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RegistrationFormSupportActivity",
+                keyColumns: new[] { "RequestHelpFormVariantID", "SupportActivityID" },
+                keyValues: new object[] { (byte)0, 1 },
+                column: "IsPreSelected",
+                value: true);
+        }
+    }
+}
