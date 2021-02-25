6e6370a9a31f1e8b177077964db5d60a98ac8b53
Mike Matthews <mike@factor-50.co.uk>
2021-02-25 14:02:24 +0000

    Adding SandboxBefrienderTraining Credential

--- GroupService/GroupService.Repo/Helpers/CredentialExtensions.cs
+++ GroupService/GroupService.Repo/Helpers/CredentialExtensions.cs
@@ -13,6 +13,7 @@ namespace GroupService.Repo.Helpers
         private const int IDENTITY_VERIFIED_BY_YOTI = -1;
         private const int MANUALLY_VERIFIED = 1;
         private const int DBS_CHECK = 2;
+        private const int SANDBOX_BEFRIENDER_TRAINING = 3;
         private const int AGEUKWIRRAL_DBS_CHECK_CREDENTIAL_SET = 71;
         private const int AGEUKNWK_DBS_CHECK_CREDENTIAL_SET = 91;
         private const int AGEUKSKC_DBS_CHECK_CREDENTIAL_SET = 111;
@@ -67,6 +68,12 @@ namespace GroupService.Repo.Helpers
             {
                 Id = DBS_CHECK,
                 Name = "DBS Check"
+            });
+
+            entity.HasData(new Credential
+            {
+                Id = SANDBOX_BEFRIENDER_TRAINING,
+                Name = "Sandbox - Befriender Training"
             });
         }
 
