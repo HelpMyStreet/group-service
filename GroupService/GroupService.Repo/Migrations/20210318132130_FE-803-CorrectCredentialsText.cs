9eea8200fdae12969a65662d6dc1d0ccdff658e2
Mike Matthews <mike@factor-50.co.uk>
2021-03-18 13:21:54 +0000

    FE-803 How To Achieve text correction

---
+++ GroupService/GroupService.Repo/Migrations/20210318132130_FE-803-CorrectCredentialsText.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class FE803CorrectCredentialsText : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "GroupCredential",
+                keyColumns: new[] { "GroupID", "CredentialID" },
+                keyValues: new object[] { -24, 1 },
+                column: "HowToAchieve",
+                value: "Email Sam Kelly on mailto:sam@bridgescommunitytrust.org.uk to arrange a manual ID check at The One Stop Shop.");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "GroupCredential",
+                keyColumns: new[] { "GroupID", "CredentialID" },
+                keyValues: new object[] { -24, 1 },
+                column: "HowToAchieve",
+                value: "Email the Oh My Nottz team at mailto:guy@mynottz.com to arrange a manual ID check at The One Stop Shop.");
+        }
+    }
+}
