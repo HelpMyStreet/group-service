4422518a63354f9ba019d34632240a749e495064
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-03-24 09:05:37 +0000

    remove preselected shopping for cardiff

---
+++ GroupService/GroupService.Repo/Migrations/20210324090437_RemovePreSelectedShoppingForCardiff.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class RemovePreSelectedShoppingForCardiff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RegistrationFormSupportActivity",
+                keyColumns: new[] { "RequestHelpFormVariantID", "SupportActivityID" },
+                keyValues: new object[] { (byte)12, 1 },
+                column: "IsPreSelected",
+                value: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RegistrationFormSupportActivity",
+                keyColumns: new[] { "RequestHelpFormVariantID", "SupportActivityID" },
+                keyValues: new object[] { (byte)12, 1 },
+                column: "IsPreSelected",
+                value: true);
+        }
+    }
+}
