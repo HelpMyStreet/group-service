f0ca82a9f04bc26028e0ec4289e9775e25c17be9
Mike Matthews <mike@factor-50.co.uk>
2021-04-01 11:06:40 +0100

    FE-834 Linking "Volunteer Induction" credential set to activities

---
+++ GroupService/GroupService.Repo/Migrations/20210401100549_FE-834-LinkTrainingCredentialSet.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class FE834LinkTrainingCredentialSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                columns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                values: new object[,]
+                {
+                    { -23, 1, 234 },
+                    { -23, 7, 234 },
+                    { -23, 26, 234 },
+                    { -23, 2, 234 },
+                    { -23, 16, 234 },
+                    { -23, 27, 234 },
+                    { -23, 22, 234 },
+                    { -23, 11, 234 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -23, 1, 234 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -23, 2, 234 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -23, 7, 234 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -23, 11, 234 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -23, 16, 234 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -23, 22, 234 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -23, 26, 234 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -23, 27, 234 });
+        }
+    }
+}
