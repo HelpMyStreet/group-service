4af16e1f9eca4d9f5d7fe45f81ba4763504445b1
Mike Matthews <mike@factor-50.co.uk>
2021-04-26 11:26:11 +0100

    FE-847 Rename Stanford to Four Counties PCN

---
+++ GroupService/GroupService.Repo/Migrations/20210426102524_FE-847-RenameFourCountiesPCN.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class FE847RenameFourCountiesPCN : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "Group",
+                keyColumn: "Id",
+                keyValue: -17,
+                columns: new[] { "GroupKey", "GroupName", "ParentGroupId" },
+                values: new object[] { "four-counties-pcn", "Four Counties PCN", -12 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "Group",
+                keyColumn: "Id",
+                keyValue: -17,
+                columns: new[] { "GroupKey", "GroupName", "ParentGroupId" },
+                values: new object[] { "pcn-stamford", "PCN Stamford", null });
+        }
+    }
+}
