e049c543be38d47db83a8f231fcadd84b0990166
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-04-26 14:10:23 +0100

    rename age uk

---
+++ GroupService/GroupService.Repo/Migrations/20210426130907_CorrectAgeUKSpelling.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class CorrectAgeUKSpelling : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "Group",
+                keyColumn: "Id",
+                keyValue: -9,
+                column: "GroupName",
+                value: "Age UK North West Kent");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "Group",
+                keyColumn: "Id",
+                keyValue: -9,
+                column: "GroupName",
+                value: "AgeUK North West Kent");
+        }
+    }
+}
