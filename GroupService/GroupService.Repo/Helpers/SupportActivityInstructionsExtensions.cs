21b180800494dbeb834efd79a2dd88abbc07dd27
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-04-26 13:21:06 +0100

    Add Stamford instructions

--- GroupService/GroupService.Repo/Helpers/SupportActivityInstructionsExtensions.cs
+++ GroupService/GroupService.Repo/Helpers/SupportActivityInstructionsExtensions.cs
@@ -987,9 +987,44 @@ namespace GroupService.Repo.Helpers
                         $"details in the “My Shifts” tab but searching for shifts with the status “Done”."
                     }
                 },
-                Close = "If for any reason you can’t complete the request before it’s due, let us know by updating the accepted request and clicking “Can’t Do”."
+                Close = "If for any reason you can’t complete the request before it’s due, let us know as soon as possible by updating the accepted request and clicking “Can’t Do” so we can find another volunteer."
             };
         }
+
+        private static Instructions GetInstructions_LincolnStamfordVaccineSupport()
+        {
+            return new Instructions()
+            {
+                SupportActivityInstructions = SupportActivityInstructionsEnum.Lincoln_Stamford_VaccineSupport,
+                ActivityDetails = $"Vaccination programme support volunteers are needed to help things run smoothly at local COVID-19 vaccination centres. " +
+                $"Duties may vary by location and could include a range of indoor or outdoor activities, " +
+                $"such as: helping patients find their way around the vaccination centre, providing assistance and reassurance to patients while they are on site," +
+                $" general domestic duties (e.g. wiping down furniture and restocking sanitisation stations). You do not need a clinical background or any specialist skills to accept " +
+                $"this role and basic training will be provided on the day. For a fuller description please see the volunteer instructions which are available when you click to accept the " +
+                $"shift or by clicking “View more info” on accepted shifts in the “My Shifts” tab.",
+                Intro = null,
+                Steps = new System.Collections.Generic.List<Step>()
+                {
+                    new Step()
+                    {
+                        Heading = "Check the request details",
+                        Detail = $"Please read the [role description and useful information](/pdf/lincs-vaccination-support/outdoor-volunteer-role-description-pcn-four-counties.pdf) " +
+                        $"attachment for full details of the role. " +
+                        $"Volunteers may also be asked to participate in regular lateral flow testing, you can find more information [here](/pdf/lincs-vaccination-support/lateral-flow-testing-for-vaccination-support-volunteers.pdf)."
+                    },
+                    new Step()
+                    {
+                        Heading = "Attending your shift",
+                        Detail = $"You’ll receive a reminder email the day before your shift. Make sure to bring a face covering and appropriate " +
+                        $"clothing for the weather (e.g. a warm / waterproof coat). On your first shift at St. Mary’s please also bring some photo ID " +
+                        $"so we can take a copy for our records. Once you’ve completed a shift it will be marked as complete automatically. " +
+                        $"You’ll still be able to find all the details in the “My Shifts” tab but searching for shifts with the status “Done”."
+                    }
+                },
+                Close = "If for any reason you can’t complete the request before it’s due, let us know as soon as possible by updating the accepted request and clicking “Can’t Do” so we can find another volunteer."
+            };
+        }
+
 
         private static Instructions GetInstructions_AgeConnectCardiff_FriendlyChat()
         {
@@ -1761,6 +1796,12 @@ namespace GroupService.Repo.Helpers
 
             entity.HasData(new EntityFramework.Entities.SupportActivityInstructions
             {
+                SupportActivityInstructionsId = (short)SupportActivityInstructionsEnum.Lincoln_Stamford_VaccineSupport,
+                Instructions = JsonConvert.SerializeObject(GetInstructions_LincolnStamfordVaccineSupport())
+            });
+
+            entity.HasData(new EntityFramework.Entities.SupportActivityInstructions
+            {
                 SupportActivityInstructionsId = (short)SupportActivityInstructionsEnum.AgeConnectCardiff_InPersonBefriending,
                 Instructions = JsonConvert.SerializeObject(GetInstructions_AgeConnectCardiff_InPersonBefriending())
             });
@@ -1969,7 +2010,7 @@ namespace GroupService.Repo.Helpers
             Populate(entity, Groups.LouthPCN, SupportActivities.VaccineSupport, SupportActivityInstructionsEnum.Lincoln_VaccineSupport);
             Populate(entity, Groups.SouthLincolnPCN, SupportActivities.VaccineSupport, SupportActivityInstructionsEnum.Lincoln_VaccineSupport);
             Populate(entity, Groups.SpilsbyPCN, SupportActivities.VaccineSupport, SupportActivityInstructionsEnum.Lincoln_VaccineSupport);
-            Populate(entity, Groups.StamfordPCN, SupportActivities.VaccineSupport, SupportActivityInstructionsEnum.Lincoln_VaccineSupport);
+            Populate(entity, Groups.StamfordPCN, SupportActivities.VaccineSupport, SupportActivityInstructionsEnum.Lincoln_Stamford_VaccineSupport);
 
             Populate(entity, Groups.LincolnshireVolunteers, SupportActivities.EmergencySupport, SupportActivityInstructionsEnum.HMS_EmergencySupport);
             Populate(entity, Groups.LincolnshireVolunteers, SupportActivities.VolunteerSupport, SupportActivityInstructionsEnum.HMS_VolunteerSupport);
