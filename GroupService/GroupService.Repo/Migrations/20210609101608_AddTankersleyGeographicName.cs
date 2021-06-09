c9cf336b92e81ff6ebf6fb892576ed172876a686
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-06-09 15:03:30 +0100

    Add group email configuration

---
+++ GroupService/GroupService.Repo/Migrations/20210609101608_AddTankersleyGeographicName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class AddTankersleyGeographicName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "Group",
+                keyColumn: "Id",
+                keyValue: -5,
+                column: "GeographicName",
+                value: "Tankersley or Pilley");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "Group",
+                keyColumn: "Id",
+                keyValue: -5,
+                column: "GeographicName",
+                value: null);
+        }
+    }
+}
