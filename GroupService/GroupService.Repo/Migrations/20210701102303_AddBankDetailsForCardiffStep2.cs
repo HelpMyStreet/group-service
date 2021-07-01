d47f42edb02294aeefe07eda28b1ce70634d4ebf
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-07-01 11:34:19 +0100

    Add bank details

---
+++ GroupService/GroupService.Repo/Migrations/20210701102303_AddBankDetailsForCardiffStep2.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class AddBankDetailsForCardiffStep2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -32, 11, 328 },
+                column: "DisplayOrder",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -32, 32, 328 },
+                column: "DisplayOrder",
+                value: 1);
+
+            migrationBuilder.InsertData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                columns: new[] { "GroupID", "ActivityID", "CredentialSetID", "DisplayOrder" },
+                values: new object[,]
+                {
+                    { -32, 32, 329, 2 },
+                    { -32, 11, 329, 2 }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Group",
+                table: "CredentialSet",
+                columns: new[] { "ID", "GroupID", "CredentialID" },
+                values: new object[] { 329, -32, 9 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -32, 11, 329 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -32, 32, 329 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "CredentialSet",
+                keyColumns: new[] { "ID", "GroupID", "CredentialID" },
+                keyValues: new object[] { 329, -32, 9 });
+
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -32, 11, 328 },
+                column: "DisplayOrder",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                schema: "Group",
+                table: "ActivityCredentialSet",
+                keyColumns: new[] { "GroupID", "ActivityID", "CredentialSetID" },
+                keyValues: new object[] { -32, 32, 328 },
+                column: "DisplayOrder",
+                value: 0);
+        }
+    }
+}
