a8a637873ce73374cb62090c805e4845ca80bbd9
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-09-15 14:39:46 +0100

    Add boston support activities instructions

---
+++ GroupService/GroupService.Repo/Migrations/20210915133847_AddBostonGNSSupportActivityInstructions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class AddBostonGNSSupportActivityInstructions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "Group",
+                table: "GroupSupportActivityConfiguration",
+                columns: new[] { "GroupID", "SupportActivityID", "Radius", "SupportActivityInstructionsID" },
+                values: new object[,]
+                {
+                    { -34, 1, 20.0, (short)1 },
+                    { -34, 7, 2000.0, (short)4 },
+                    { -34, 2, 20.0, (short)2 },
+                    { -34, 27, 20.0, (short)2 },
+                    { -34, 5, 20.0, (short)3 },
+                    { -34, 29, 20.0, (short)3 },
+                    { -34, 11, 20.0, (short)2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityConfiguration",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -34, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityConfiguration",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -34, 2 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityConfiguration",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -34, 5 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityConfiguration",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -34, 7 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityConfiguration",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -34, 11 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityConfiguration",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -34, 27 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupSupportActivityConfiguration",
+                keyColumns: new[] { "GroupID", "SupportActivityID" },
+                keyValues: new object[] { -34, 29 });
+        }
+    }
+}
