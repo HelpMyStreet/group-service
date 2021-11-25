df1379b3d8d5be486d928ae26f0983b6bc94a5df
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-11-25 12:12:17 +0000

    Add GetNewsTicker

---
+++ GroupService/GroupService.Handlers/GetNewsTickerHandler.cs
@@ -0,0 +1,60 @@
+﻿using GroupService.Core.Interfaces.Repositories;
+using HelpMyStreet.Contracts;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GroupService.Handlers
+{
+    public class GetNewsTickerHandler : IRequestHandler<NewsTickerRequest, NewsTickerResponse>
+    {
+        private readonly IRepository _repository;
+
+        public GetNewsTickerHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<NewsTickerResponse> Handle(NewsTickerRequest request, CancellationToken cancellationToken)
+        {
+            NewsTickerResponse response = new NewsTickerResponse()
+            {
+                Messages = new List<NewsTickerMessage>()
+            };
+
+            int volunteerCount = await _repository.MemberVolunterCount(request.GroupId);
+            int newVolunteerCountInLast7Days = await _repository.MemberVolunterCountLastXDays(request.GroupId, 7);
+            int newVolunteerCountInLastDay = await _repository.MemberVolunterCountLastXDays(request.GroupId, 1);
+
+            if(volunteerCount>=5)
+            {
+                response.Messages.Add(new NewsTickerMessage()
+                {
+                    Value = volunteerCount,
+                    Message = $"**{ volunteerCount }** volunteers waiting to help"
+                });
+            }
+
+            if (newVolunteerCountInLast7Days >= 2 && newVolunteerCountInLastDay<=1)
+            {
+                response.Messages.Add(new NewsTickerMessage()
+                {
+                    Value = newVolunteerCountInLast7Days,
+                    Message = $"**{ newVolunteerCountInLast7Days }** new volunteers joined this week"
+                });
+            }
+
+            if (newVolunteerCountInLastDay >1)
+            {
+                response.Messages.Add(new NewsTickerMessage()
+                {
+                    Value = newVolunteerCountInLastDay,
+                    Message = $"**{ newVolunteerCountInLastDay }** new volunteers joined today"
+                });
+            }
+
+            return response;
+        }
+    }
+}
