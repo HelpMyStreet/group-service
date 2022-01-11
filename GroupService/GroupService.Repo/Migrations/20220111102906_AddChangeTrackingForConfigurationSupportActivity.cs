63dfd1115272d205427ca95885a66326cf8bd950
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-01-11 10:38:26 +0000

    add change tracking for configuration support activity

--- GroupService/GroupService.Repo/Migrations/20220111102906_AddChangeTrackingForConfigurationSupportActivity.cs
+++ GroupService/GroupService.Repo/Migrations/20220111102906_AddChangeTrackingForConfigurationSupportActivity.cs
@@ -7,22 +7,22 @@ namespace GroupService.Repo.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-                INSERT INTO [dbo].[table_store_ChangeTracking_version]([TableName],[SYS_CHANGE_VERSION]) SELECT 'Communication.SupportActivity',0;
+                INSERT INTO [dbo].[table_store_ChangeTracking_version]([TableName],[SYS_CHANGE_VERSION]) SELECT 'Configuration.SupportActivity',0;
                 ");
 
             migrationBuilder.Sql(@"
-                ALTER TABLE [Communication].[SupportActivity] ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = ON)
+                ALTER TABLE [Configuration].[SupportActivity] ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = ON)
                 ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-                DELETE FROM [dbo].[table_store_ChangeTracking_version] WHERE TableName='Communication.SupportActivity';                
+                DELETE FROM [dbo].[table_store_ChangeTracking_version] WHERE TableName='Configuration.SupportActivity';                
                 ");
 
             migrationBuilder.Sql(@"
-                ALTER TABLE [Communication].[SupportActivity] DISABLE CHANGE_TRACKING
+                ALTER TABLE [Configuration].[SupportActivity] DISABLE CHANGE_TRACKING
                 ");
         }
     }
