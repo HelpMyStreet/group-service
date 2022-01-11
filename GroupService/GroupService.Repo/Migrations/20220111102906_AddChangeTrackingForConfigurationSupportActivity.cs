ef7c44603f9ca753cf8aaa4375f24e8a9c00de0a
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-01-11 10:32:44 +0000

    Add change tracking for configuration support activities

---
+++ GroupService/GroupService.Repo/Migrations/20220111102906_AddChangeTrackingForConfigurationSupportActivity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class AddChangeTrackingForConfigurationSupportActivity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO [dbo].[table_store_ChangeTracking_version]([TableName],[SYS_CHANGE_VERSION]) SELECT 'Communication.SupportActivity',0;
+                ");
+
+            migrationBuilder.Sql(@"
+                ALTER TABLE [Communication].[SupportActivity] ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = ON)
+                ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM [dbo].[table_store_ChangeTracking_version] WHERE TableName='Communication.SupportActivity';                
+                ");
+
+            migrationBuilder.Sql(@"
+                ALTER TABLE [Communication].[SupportActivity] DISABLE CHANGE_TRACKING
+                ");
+        }
+    }
+}
