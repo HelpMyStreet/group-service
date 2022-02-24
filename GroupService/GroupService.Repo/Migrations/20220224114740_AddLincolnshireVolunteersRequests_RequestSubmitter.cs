9e7d33429e29d4720504045c8ff9247e83967de1
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-02-24 11:51:34 +0000

    Add LincolnshireVolunteersRequests_RequestSubmitter

---
+++ GroupService/GroupService.Repo/Migrations/20220224114740_AddLincolnshireVolunteersRequests_RequestSubmitter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class AddLincolnshireVolunteersRequests_RequestSubmitter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {            
+            migrationBuilder.InsertData(
+                schema: "Lookup",
+                table: "RequestHelpFormVariant",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {         
+                    { 32, "LincolnshireVolunteersRequests_RequestSubmitter" }
+                });
+
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -12, "a" },
+                column: "RequestHelpFormVariant",
+                value: (byte)32);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Lookup",
+                table: "RequestHelpFormVariant",
+                keyColumn: "ID",
+                keyValue: 32);
+
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -12, "a" },
+                column: "RequestHelpFormVariant",
+                value: (byte)18);
+        }
+    }
+}
