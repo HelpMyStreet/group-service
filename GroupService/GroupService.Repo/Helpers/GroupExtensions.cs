b20b9b6b8a73acb5baba26adef6dc1b849047b68
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-03-28 10:42:56 +0100

    Edit JoinUpText for Ukraine Group

--- GroupService/GroupService.Repo/Helpers/GroupExtensions.cs
+++ GroupService/GroupService.Repo/Helpers/GroupExtensions.cs
@@ -317,9 +317,17 @@ namespace GroupService.Repo.Helpers
                 LinkURL = "/help-ukraine",
                 ShortName = "Help Ukraine",
                 GeographicName = "Help Ukraine",
-                JoinGroupPopUpDetail = "To become a host / sponsor for people forced to escape their homeland you will need to be able to provide at least six months’ accommodation and can be living in any part of the UK. You must be over 18, and can be of any nationality, with any immigration status, providing you have at least 6 months’ leave to remain in the UK." + Environment.NewLine + Environment.NewLine +
-                "To ensure a safe and suitable environment for  those requiring accommodation, we’re going to be carrying out checks (background and in-person) on you as a sponsor, your family and the accommodation that you will provide." + Environment.NewLine + Environment.NewLine +
-                "Please only continue with your registration if you can meet these terms."
+                JoinGroupPopUpDetail = $"This group has been created to help prospective UK hosts find a suitable match for the UK Government Homes " +
+                $"for Ukraine Scheme. Please only register if the following apply:" +
+                $"<ul>" +
+                $"<li>You are 18 or over</li>" +
+                $"<li>You are able to offer accommodation for at least 6 months</li>" +
+                $"<li>You are a British citizen or have leave to remain in the UK for at least 6 months</li>" +
+                $"<li>You are looking to be a sponsor for UK Government Homes for Ukraine Scheme</li>" +
+                $"<li>You will submit a separate application to the UK Government Homes for Ukraine Scheme once a match has been confirmed</li>" +
+                $"</ul>" +
+                $"You will need to verify your identity with photographic ID to access a match using this site. The UK Government may also carry out their own " +
+                $"checks following an application to the scheme. Please only continue with your registration if you can meet these terms."
             });
         }
 
