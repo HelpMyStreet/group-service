88b40d5ffd2d4892f3047ae37117a4f076d90095
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-06-14 08:32:37 +0100

    Remove ukraine refugees group

---
+++ GroupService/GroupService.Repo/Migrations/20220613150352_RemoveUkraineUsers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class RemoveUkraineUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM [Group].[UserRole]
+                WHERE   GroupID=-35
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
---
+++ GroupService/GroupService.Repo/Migrations/20220613153544_RemoveUkraineUsers_Step4.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class RemoveUkraineUsers_Step4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM [Group].[UserCredential]
+                WHERE   GroupID=-35
+            ");
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupCredential",
+                keyColumns: new[] { "GroupID", "CredentialID" },
+                keyValues: new object[] { -35, -1 });
+
+            migrationBuilder.DeleteData(
+                schema: "Group",
+                table: "GroupCredential",
+                keyColumns: new[] { "GroupID", "CredentialID" },
+                keyValues: new object[] { -35, 11 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "Group",
+                table: "GroupCredential",
+                columns: new[] { "GroupID", "CredentialID", "CredentialTypeID", "CredentialVerifiedById", "DisplayOrder", "HowToAchieve", "HowToAchieve_CTA_Destination", "Name", "WhatIsThis" },
+                values: new object[] { -35, -1, (byte)1, (byte)1, 1, "Complete online", "/account?next=verify", "Yoti ID Verification", "Yoti is our trusted digital identity verification provider. Volunteers can follow the instructions in their profile to get verified. Volunteer admins cannot edit this credential." });
+
+            migrationBuilder.InsertData(
+                schema: "Group",
+                table: "GroupCredential",
+                columns: new[] { "GroupID", "CredentialID", "CredentialTypeID", "CredentialVerifiedById", "DisplayOrder", "HowToAchieve", "HowToAchieve_CTA_Destination", "Name", "WhatIsThis" },
+                values: new object[] { -35, 11, (byte)2, (byte)2, 2, "Hosts must be approved before they can be matched to a request for accommodation. A member of our team will be in touch as soon as possible to start the process.", "", "Approved Host", "Use this credential to certify that a host has completed all of the necessary checks and is approved for hosting. Volunteer admins should follow internal processes for manually verifying an approved host." });
+        }
+    }
+}
