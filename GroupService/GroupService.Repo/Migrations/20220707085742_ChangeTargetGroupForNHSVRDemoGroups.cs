00844b7d9d854162d4c0d681ea517d621b273f1c
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-07-07 10:02:56 +0100

    ChangeTargetGroupForNHSVRDemoGroups

---
+++ GroupService/GroupService.Repo/Migrations/20220707085742_ChangeTargetGroupForNHSVRDemoGroups.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class ChangeTargetGroupForNHSVRDemoGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -40, "a" },
+                column: "TargetGroups",
+                value: (byte)3);
+
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -39, "a" },
+                column: "TargetGroups",
+                value: (byte)3);
+
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -38, "a" },
+                column: "TargetGroups",
+                value: (byte)1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -40, "a" },
+                column: "TargetGroups",
+                value: (byte)0);
+
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -39, "a" },
+                column: "TargetGroups",
+                value: (byte)0);
+
+            migrationBuilder.UpdateData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -38, "a" },
+                column: "TargetGroups",
+                value: (byte)0);
+        }
+    }
+}
