979b3463a52157a23d4e1a23262c10224f04ec53
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2022-11-29 08:35:44 +0000

    Change RequestorDetails for -37

--- GroupService/GroupService.Repo/Migrations/20221129083149_ChangeRequestorDetailsFor37.Designer.cs
+++ GroupService/GroupService.Repo/Migrations/20221129083149_ChangeRequestorDetailsFor37.Designer.cs
@@ -4,14 +4,16 @@ using GroupService.Repo;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GroupService.Repo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221129083149_ChangeRequestorDetailsFor37")]
+    partial class ChangeRequestorDetailsFor37
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
