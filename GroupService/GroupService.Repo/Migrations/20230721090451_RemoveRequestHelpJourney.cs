095406f8a8da2a3ccae14b0ee46d8ed449127e7b
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2023-07-21 10:10:02 +0100

    Remove RequestHelpJourney

---
+++ GroupService/GroupService.Repo/Migrations/20230721090451_RemoveRequestHelpJourney.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupService.Repo.Migrations
+{
+    public partial class RemoveRequestHelpJourney : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -40, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -39, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -38, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -37, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -36, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -34, "" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -34, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -33, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -32, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -23, "" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -23, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -20, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -17, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -14, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -13, "" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -13, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -12, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -11, "" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -11, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -10, "" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -9, "" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -9, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -8, "" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -5, "" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -3, "a" });
+
+            migrationBuilder.DeleteData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                keyColumns: new[] { "GroupID", "Source" },
+                keyValues: new object[] { -2, "" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "Website",
+                table: "RequestHelpJourney",
+                columns: new[] { "GroupID", "Source", "AccessRestrictedByRole", "RequestHelpFormVariant", "RequestorDefinedByGroup", "RequestsRequireApproval", "SuppressRecipientPersonalDetails", "TargetGroups" },
+                values: new object[,]
+                {
+                    { -2, "", false, (byte)4, false, false, false, (byte)1 },
+                    { -38, "a", true, (byte)34, false, false, false, (byte)0 },
+                    { -37, "a", true, (byte)32, true, false, true, (byte)4 },
+                    { -36, "a", true, (byte)32, true, false, true, (byte)4 },
+                    { -34, "a", true, (byte)30, false, false, true, (byte)0 },
+                    { -34, "", false, (byte)29, false, false, true, (byte)0 },
+                    { -33, "a", true, (byte)28, false, false, true, (byte)0 },
+                    { -32, "a", true, (byte)26, true, false, false, (byte)0 },
+                    { -23, "a", true, (byte)20, false, false, null, (byte)4 },
+                    { -23, "", true, (byte)21, false, true, true, (byte)4 },
+                    { -20, "a", true, (byte)17, true, false, false, (byte)4 },
+                    { -17, "a", true, (byte)17, true, false, false, (byte)4 },
+                    { -14, "a", true, (byte)17, true, false, false, (byte)4 },
+                    { -12, "a", true, (byte)32, false, false, false, (byte)4 },
+                    { -13, "a", true, (byte)16, true, false, false, (byte)4 },
+                    { -13, "", false, (byte)15, false, true, false, (byte)4 },
+                    { -11, "a", true, (byte)14, true, false, false, (byte)4 },
+                    { -11, "", false, (byte)13, false, true, false, (byte)4 },
+                    { -10, "", false, (byte)12, false, false, false, (byte)4 },
+                    { -9, "a", true, (byte)11, true, false, false, (byte)4 },
+                    { -9, "", false, (byte)10, false, true, false, (byte)4 },
+                    { -8, "", false, (byte)9, false, false, false, (byte)4 },
+                    { -5, "", false, (byte)1, false, false, false, (byte)4 },
+                    { -3, "a", true, (byte)2, true, false, true, (byte)4 },
+                    { -39, "a", true, (byte)34, false, false, false, (byte)0 },
+                    { -40, "a", true, (byte)34, false, false, false, (byte)0 }
+                });
+        }
+    }
+}
